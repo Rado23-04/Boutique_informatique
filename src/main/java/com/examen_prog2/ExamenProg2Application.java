9e8a1dff2ff4ac8ca400cc565f1b30f25d656d8e
rado <hei.rado.2@gmail.com>
2023-08-23 16:21:51 +0300

    fix: add componentScan

--- src/main/java/com/examen_prog2/ExamenProg2Application.java
+++ src/main/java/com/examen_prog2/ExamenProg2Application.java
@@ -2,10 +2,12 @@ package com.examen_prog2;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
 
 @SpringBootApplication
+@ComponentScan({"com.examen_prog2","Controller"})
 public class ExamenProg2Application {
-
     public static void main(String[] args) {
         SpringApplication.run(ExamenProg2Application.class, args);
     }
