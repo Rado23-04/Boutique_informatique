df96625dcc1f50374cd5d8a9ff0afb9efd8534b2
rado <hei.rado.2@gmail.com>
2023-08-23 09:51:50 +0300

    fix: update genericDAO

--- src/main/java/ConnectionDatabase/GenericDAO.java
+++ src/main/java/ConnectionDatabase/GenericDAO.java
@@ -16,4 +16,11 @@ public abstract class GenericDAO<Z> {
     }
 
     public abstract List<Client> findAll() throws SQLException;
+
+    public abstract void delete(int id) throws SQLException;
+
+    public abstract void selectById(int id_client) throws SQLException;
+
+
+    public abstract void updateClient(Client update) throws SQLException;
 }
